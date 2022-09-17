652dbadc457e54a64e15e54c0767b35c58bd84f0
Chandan Ojha <chandanojha776@gmail.com>
2022-09-17 14:30:57 +0600

    Array concat() method practice done

---
+++ Array Concat Method/script.js
@@ -0,0 +1,7 @@
+var numbers1 = [1,2,3,4,5];
+var numbers2 = [6,7,8,9,10];
+var numbers3 = [11,12,13,14,15];
+
+var result = numbers1.concat(numbers2,numbers3);
+console.log(numbers1)
+console.log(result);
