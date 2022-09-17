a887bd75b865d8939e4c79a52519c7c8ffc6f514
Chandan Ojha <chandanojha776@gmail.com>
2022-09-17 14:33:50 +0600

    updated

--- Array Splice Method/script.js
+++ Array Splice Method/script.js
@@ -1,5 +1,5 @@
 //Array.prototype.splice()
 var numbers = [1,2,3,4,5];
-var result = numbers.splice(-1,2,10,11,12,13,14,15);
+var result = numbers.splice(-1,2,10,11,12,13,14,15); //splice method changes the main array
 console.log(numbers);
 console.log(result);
