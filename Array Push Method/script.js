4f857c08de36a97acdd6361ba8c4b4c4afbf20d6
Chandan Ojha <chandanojha776@gmail.com>
2022-09-17 14:46:43 +0600

    Array push method practice done

---
+++ Array Push Method/script.js
@@ -0,0 +1,7 @@
+//Array.prototype.push()
+var numbers = [1,2,3,4,5];
+
+//numbers.push(6,7,8,9); //array push method change the main array
+var result = numbers.push(6,7,8,9);
+
+console.log(result);
