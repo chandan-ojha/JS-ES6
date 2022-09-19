80cb8851a1af98da752f82fbad906a50c6d7838e
Chandan Ojha <chandanojha776@gmail.com>
2022-09-19 09:11:32 +0600

    Important object tricks practice done

---
+++ Important object tricks/script.js
@@ -0,0 +1,40 @@
+//Important Object Methods
+
+/*var myObj = {
+    name: "JavaScript",
+    founder: "Brendan Eich",
+    estd: "1995",
+    ranking : 1,
+};
+
+var keys = Object.keys(myObj);
+var values = Object.values(myObj);
+var entries = Object.entries(myObj);
+
+//console.log(keys);
+//console.log(values);
+console.log(entries);*/
+
+
+//Object Shorthand
+var x = 4;
+var y = 5;
+var z = x * y;
+
+var myObj = {
+    name: "JavaScript",
+    founder: "Brendan Eich",
+    estd: "1995",
+    ranking : 1,
+    x, //x: x
+    y, //y: y
+    z  //z: z
+};
+
+var keys = Object.keys(myObj);
+var values = Object.values(myObj);
+var entries = Object.entries(myObj);
+
+//console.log(keys);
+//console.log(values);
+console.log(entries);
