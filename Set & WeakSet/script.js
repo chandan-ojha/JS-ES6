9b3f7b6232d112eb08515508abd803a62b27a758
Chandan Ojha <chandanojha776@gmail.com>
2022-09-21 16:40:37 +0600

    find unique element form array by using set

--- Set & WeakSet/script.js
+++ Set & WeakSet/script.js
@@ -38,15 +38,50 @@ for(let value of mySet1){
 
 //convert Sets to Array
 //let myArray = [1,2,3];
-let myArray = "Bangladesh";
-let mySet = new Set(myArray);
+//let myArray = "Bangladesh";
+//let mySet = new Set(myArray);
 //log([...mySet]);
-mySet.add(4);
-log(Array.from(mySet));
+//mySet.add(4);
+//log(Array.from(mySet));
 
 //~ 16:13 second Completed
 
 //set usage
+/*let myArray = [1,2,3];
+
+let mySet = new Set(myArray);
+mySet.add({
+    a: 5,
+    b: 2
+});
+
+mySet.add({
+    a: 5,
+    b: 2
+});
+
+let object = {
+    a: 9,
+    b: 10
+}
+
+
+mySet.add(object);
+mySet.add(object); //~Set always allow unique value
+mySet.add(object);
+
+log(mySet);*/
+
+//Example-01
+
+//find unique element using by set
+let myArray = [1, 2, 3, 4, 5, 5, 5, 6, 5];
+
+/* first of all this array convert to a set element and
+remove the duplicate value and
+then the set element convert to a array */
+
+log([...new Set(myArray)]);
 
 
 
