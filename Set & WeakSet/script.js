42e4480ed471c1d76c29d20217655d24aac91663
Chandan Ojha <chandanojha776@gmail.com>
2022-09-21 14:36:40 +0600

    Set and WeakSet practice ongoing

---
+++ Set & WeakSet/script.js
@@ -0,0 +1,51 @@
+//Set & WeakSet
+
+function log(anything){
+    return console.log(anything);
+}
+
+//Array Declaration
+//let myArray = [] //literal syntax
+//let myArray = new Array();
+//myArray[0] = 5;
+//myArray[1] = 2;
+//log(myArray);
+
+//Set Declaration
+//let mySet = new Set();
+/*mySet.add(15);
+mySet.add("Bangladesh");
+mySet.delete("Bangladesh");*/
+//mySet.add(5).add(6).add("Bangladesh").delete(5);
+//log(mySet.size);
+
+//mySet.clear();
+//log(mySet.has("Bangladesh"));
+//log(mySet);
+
+//Array Convert to Set
+/*let myArray = [1,2,3];
+let mySet = new Set(myArray);
+log(mySet);*/
+
+//set is iterable
+/*let mySet1 = new Set("Bangladesh");
+for(let value of mySet1){
+    log(value);
+}*/
+//log(mySet1);
+
+
+//convert Sets to Array
+//let myArray = [1,2,3];
+let myArray = "Bangladesh";
+let mySet = new Set(myArray);
+//log([...mySet]);
+mySet.add(4);
+log(Array.from(mySet));
+
+//set usage
+
+
+
+
